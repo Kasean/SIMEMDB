269bc67d2f8571064fb57f70be2a1ee8b01eef58
Kasean <serguei.kakichev@mail.ru>
2022-07-26 10:52:54 +0300

    feat: replace string literals to request keywords

--- src/main/scala/requestprocessor/CreateProcessor.scala
+++ src/main/scala/requestprocessor/CreateProcessor.scala
@@ -8,37 +8,54 @@ import java.util.UUID
 
 class CreateProcessor {
 
-  def createStorage(request: String): Storage =
-
-    val name = request.replace("WITH NAME '", "")
-      .replace("'", "")
-    StorageBuffer.createStorage(name)
-
+  def createStorage(request: String): Storage = {
+    
+    val storageName = request.replace(RequestKeyWords.WITH.name() + " "
+      + RequestKeyWords.NAME.name() + " '", "")
+    StorageBuffer.createStorage(storageName)
+  }
+  
   def createDocument(request: String): Document = {
-
-    val names = request.split(" IN STORAGE WITH NAME ")
+    
+    val names = request.split(" " + RequestKeyWords.IN.name() + " " 
+      + RequestKeyWords.STORAGE.name() + " " 
+      + RequestKeyWords.WITH.name() + " " 
+      + RequestKeyWords.NAME.name() + " ")
     val storageName = names(1).replace("'", "").replace("'", "")
     val storage = StorageBuffer.getStorage(storageName)
-    val docName = names(0).replace("WITH NAME ", "").replace("'", "").replace("'", "")
+    val docName = names(0)
+      .replace(RequestKeyWords.WITH.name() + " " + RequestKeyWords.NAME.name() + " ", "")
+      .replace("'", "")
+      .replace("'", "")
     storage.createDocument(docName)
   }
 
   def createRecord(request: String): UUID = {
-    
-    val startContentIndex = request.indexOf("WITH CONTENT '")
-    val endContentIndex = request.indexOf("' IN DOCUMENT")
+
+    val startContentIndex = request.indexOf(RequestKeyWords.WITH.name() + " " + RequestKeyWords.CONTENT.name() + " '")
+    val endContentIndex = request.indexOf("' " + RequestKeyWords.IN.name() + " " +  RequestKeyWords.DOCUMENT.name())
     val recordContent = request.substring(startContentIndex, endContentIndex)
       .replace(RequestKeyWords.WITH.name() + " " + RequestKeyWords.CONTENT.name() + " '", "")
 
-    val startDocNameIndex = request.indexOf("DOCUMENT WITH NAME '")
-    val endDocNameIndex = request.indexOf("' IN STORAGE")
+    val startDocNameIndex = request.indexOf(RequestKeyWords.DOCUMENT.name + " "
+      + RequestKeyWords.WITH.name() + " "
+      + RequestKeyWords.NAME.name() + " '")
+    val endDocNameIndex = request.indexOf("' " + RequestKeyWords.IN.name() + " " +  RequestKeyWords.STORAGE.name())
     val docName = request.substring(startDocNameIndex, endDocNameIndex)
-      .replace(RequestKeyWords.DOCUMENT.name + " " + RequestKeyWords.WITH.name() + " " + RequestKeyWords.NAME.name() + " '", "")
+      .replace(RequestKeyWords.DOCUMENT.name + " " 
+        + RequestKeyWords.WITH.name() + " " 
+        + RequestKeyWords.NAME.name() + " '", "")
 
-    val startStorageNameIndex = request.indexOf("IN STORAGE WITH NAME '")
+    val startStorageNameIndex = request.indexOf(RequestKeyWords.IN.name() + " "
+      + RequestKeyWords.STORAGE.name() + " "
+      + RequestKeyWords.WITH.name() + " "
+      + RequestKeyWords.NAME.name() + " '")
     val endStorageNameIndex = request.length - 1
     val storageName = request.substring(startStorageNameIndex, endStorageNameIndex)
-      .replace(RequestKeyWords.IN.name() + " " + RequestKeyWords.STORAGE.name() + " " + RequestKeyWords.WITH.name() + " " + RequestKeyWords.NAME.name() + " '", "")
+      .replace(RequestKeyWords.IN.name() + " " 
+        + RequestKeyWords.STORAGE.name() + " " 
+        + RequestKeyWords.WITH.name() + " " 
+        + RequestKeyWords.NAME.name() + " '", "")
 
     StorageBuffer.getStorage(storageName)
       .getDocument(docName)
